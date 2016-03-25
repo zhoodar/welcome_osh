9eb27142ad3769049c0f22aaec056078cd093166
ZHoodar <ajoodar@gmail.com>
2016-03-25 17:47:25 +0600

    added new function get_cat for menu

--- db_funs.php
+++ db_funs.php
@@ -8,12 +8,41 @@
         $db = 'base_search';
         
         $connection = mysql_connect($host, $user, $pswd);
-        mysql_query("SET NAMES utf8");
+        mysql_query("SET NAME Utf-8");
         if(!$connection || !mysql_select_db($db,$connection))
         {
             return false;
         }
         return $connection;
     }
+	
+	function db_result_to_array($result)
+    {
+        $res_array = array();
+        
+        $count = 0;
+        
+        while($row = mysql_fetch_array($result))
+        {
+            $res_array[$count] = $row;
+            $count++;
+        }
+        return $res_array;
+    }
+	
+	
+	function get_cat()
+    {
+        db_connect();
+        
+        $query = "SELECT * FROM ot_ra_categories ORDER BY id ";
+        
+        $result = mysql_query($query);
+        
+        $result = db_result_to_array($result);
+        
+        return $result;
+    }
+	
 
 ?>
