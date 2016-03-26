ccd21a613695adf0b368c6992d7cb5c2deebf73c
ZHoodar <ajoodar@gmail.com>
2016-03-26 11:37:51 +0600

    new page organization, to arrange array and loop

---
+++ views/pages/organization.php
@@ -0,0 +1,36 @@
+	
+<div id="workArea">     <!-- див рабочой зоны  -->
+		<a href="index.php?view=index">Главная </a>&rarr;<a href="index.php?view=torgovye-sentr">Торговые центры</a> &rarr;
+		<p> <h2>  Торговый центр ОШ Сити в ОШе. Отзывы, жалобы, контакты</h2> </p>
+		<hr size="6" color="#468bea"/>
+		
+		<div>
+		    <strong>Адрес:</strong> г.ОШ ул. Масалиева 29 
+			<br> <strong>Телефон:</strong> 0778555 444
+			<br> <strong>Часы работы:</strong> ежедневно, 09:00-22:00 
+		</div>
+
+	  <br/><br/>
+	
+	<table border="0">
+	    <tr height="150">
+	      <td><div style="padding: 2px;"><a rel="group" href="img/oshcity/3_big.jpg" ><img src="img/oshcity/3.jpg"/></a></div></td>
+	      <td><div style="padding: 2px;"><a rel="group" href="img/oshcity/2_big.jpg" ><img src="img/oshcity/2.jpg"/></a></div></td>
+	      <td><div style="padding: 2px;"><a rel="group" href="img/oshcity/1_big.jpg" ><img src="img/oshcity/1.jpg"/></a></div></td>
+	    </tr>
+	</table>
+	
+	<div>
+	<p >  <strong>Место нахождение на карте</strong> </p>
+		<img border="3" style="border-color: #adafb4;" src="img/map.png"/>
+	</div>
+	<br/>
+	
+	<div align="justify">
+	<?
+	include("comments/comment.php");
+	?>	
+	</div>
+
+	
+</div>
