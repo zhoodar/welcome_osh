0efba94dff22d982f59d0cbbc4458c157bebf254
ZHoodar <ajoodar@gmail.com>
2016-04-01 21:06:10 +0600

    added the button add organization

--- views/pages/guesthouse.php
+++ views/pages/guesthouse.php
@@ -1,111 +1,123 @@
 <div id="workArea">                                    <!-- див рабочой зоны  -->
 <a href="index.php?view=index">Главная &rarr;</a> Гостиницы &rarr;
 
-<div >
+	<div >
+			
+		<table border="0">
+		   <tr>
+		     <td width="160"><img src="img/gostinicy-novorossijska.png"  /></td> 
+		     <td  > <h2>   Гостиницы ОШа - отзывы и контакты, адреса </h2> Вашему вниманию в данном каталоге <strong>«Гостиницы ОШа»</strong> предложена достоверная и точная информация о месторасположении на карте любой интересующей гостиницы ее телефонами, адресами, часами работы и ссылками на официальные сайты. Так же можно просмотреть фотографии и отзывы о гостиницах ОШа. Здесь Вы легко сможете найти место для временного пребывания в г. ОШ.	</td>
+		 </tr>
+		 <tr>
+		    <td width="120">&nbsp;</td>
+		 	<td>
+   				<div  align="center" >
+					<form action="add_organization.php" method="POST" class="decorated-form">
+						<input name="news" type="submit" value="Добавить свою организацию" class="orange-button">
+					</form>
+				</div>
+	        </td>
+		 </tr>
+		 </table>
+		
+		
+	</div>
 	
-<table border="0">
-   <tr>
-    <td width="160"><img src="img/gostinicy-novorossijska.png"  /></td> 
-   <td  > <h2>   Гостиницы ОШа - отзывы и контакты, адреса </h2> Вашему вниманию в данном каталоге <strong>«Гостиницы ОШа»</strong> предложена достоверная и точная информация о месторасположении на карте любой интересующей гостиницы ее телефонами, адресами, часами работы и ссылками на официальные сайты. Так же можно просмотреть фотографии и отзывы о гостиницах ОШа. Здесь Вы легко сможете найти место для временного пребывания в г. ОШ.	</td>
- </tr>
- </table>
-	
-</div>
-<br />
-<div id="kino">
-	<table align="center"  cellspacing="5" bgcolor="#ced7dd">
-		<tr>
-			<td bgcolor="#ced7dd" height="30"> <u>Статистика справочника «Гостиныцы ОШа»</u></td>
-		</tr>
-		<tr   >
-			<td>
-             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
-			 <a href="oshnuru.php"><strong>Гостиница "Ош-Нуру" </strong> </a> <br> <strong>Адрес:</strong> г. Ош  ул. Баялинова 1, 723500
-			<br> <strong>Телефон:</strong> 0778565854
-			<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
-			<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
-			</div>
-			 </td>
-		</tr>
-		
-		<tr   >
-			<td>
-             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
-			 <a href="#"><strong>Гостиница  Classic </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. Алиева 143, 723500  
-			<br> <strong>Телефон:</strong> 0555424568
-			<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
-			<br> <a href="#"><strong>Фотографий </strong></a> |<a href="#"><strong>Отзывы</strong></a>
-			</div>
-			 </td>
-		</tr>
-		
-		<tr   >
-			<td>
-             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
-			 <a href="#"><strong>Гостиница  Sunrise Osh </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. А. Масалиева 25A, 723500  
-			<br> <strong>Телефон:</strong> 0555124565
-			<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
-			<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
-			</div>
-			 </td>
-		</tr>
-		
-		<tr   >
-			<td>
-             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
-			 <a href="#"><strong>Гостиница  "ТЕЗ"    </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. Сайбою 5,  723500  
-			<br> <strong>Телефон:</strong> 0555158565
-			<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
-			<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
-			</div>
-			 </td>
-		</tr>
-		
-		<tr   >
-			<td>
-             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
-			 <a href="#"><strong>Sunrise Guesthouse </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. А. Масалиева 105 б, 723500  
-			<br> <strong>Телефон:</strong> 0555124565
-			<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
-			<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
-			</div>
-			 </td>
-		</tr>
-		
-		<tr   >
-			<td>
-             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
-			 <a href="#"><strong>Гостиница "Орто Азия" </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул.Разакова Авеню 23, 723500  
-			<br> <strong>Телефон:</strong> 0555332565
-			<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
-			<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
-			</div>
-			 </td>
-		</tr>
-		
-		<tr   >
-			<td>
-             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
-			 <a href="#"><strong>Гостиница Silk Way Boutique  </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. Авецина 10, 723500  
-			<br> <strong>Телефон:</strong> 0555100565
-			<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
-			<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
-			</div>
-			 </td>
-		</tr>
-		
-		<tr   >
-			<td>
-             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
-			 <a href="#"><strong>Гостевой Дом 5я Авеню </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. Ленина 5, 714000   
-			<br> <strong>Телефон:</strong> 0555125675
-			<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
-			<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
-			</div>
-			 </td>
-		</tr>
-		
-	</table>
-</div>
+	<br />
+	<div id="kino">
+		<table align="center"  cellspacing="5" bgcolor="#ced7dd">
+			<tr>
+				<td bgcolor="#ced7dd" height="30"> <u>Статистика справочника «Гостиныцы ОШа»</u></td>
+			</tr>
+			<tr   >
+				<td>
+	             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
+				 <a href="oshnuru.php"><strong>Гостиница "Ош-Нуру" </strong> </a> <br> <strong>Адрес:</strong> г. Ош  ул. Баялинова 1, 723500
+				<br> <strong>Телефон:</strong> 0778565854
+				<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
+				<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
+				</div>
+				 </td>
+			</tr>
+			
+			<tr   >
+				<td>
+	             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
+				 <a href="#"><strong>Гостиница  Classic </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. Алиева 143, 723500  
+				<br> <strong>Телефон:</strong> 0555424568
+				<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
+				<br> <a href="#"><strong>Фотографий </strong></a> |<a href="#"><strong>Отзывы</strong></a>
+				</div>
+				 </td>
+			</tr>
+			
+			<tr   >
+				<td>
+	             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
+				 <a href="#"><strong>Гостиница  Sunrise Osh </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. А. Масалиева 25A, 723500  
+				<br> <strong>Телефон:</strong> 0555124565
+				<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
+				<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
+				</div>
+				 </td>
+			</tr>
+			
+			<tr   >
+				<td>
+	             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
+				 <a href="#"><strong>Гостиница  "ТЕЗ"    </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. Сайбою 5,  723500  
+				<br> <strong>Телефон:</strong> 0555158565
+				<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
+				<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
+				</div>
+				 </td>
+			</tr>
+			
+			<tr   >
+				<td>
+	             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
+				 <a href="#"><strong>Sunrise Guesthouse </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. А. Масалиева 105 б, 723500  
+				<br> <strong>Телефон:</strong> 0555124565
+				<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
+				<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
+				</div>
+				 </td>
+			</tr>
+			
+			<tr   >
+				<td>
+	             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
+				 <a href="#"><strong>Гостиница "Орто Азия" </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул.Разакова Авеню 23, 723500  
+				<br> <strong>Телефон:</strong> 0555332565
+				<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
+				<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
+				</div>
+				 </td>
+			</tr>
+			
+			<tr   >
+				<td>
+	             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
+				 <a href="#"><strong>Гостиница Silk Way Boutique  </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. Авецина 10, 723500  
+				<br> <strong>Телефон:</strong> 0555100565
+				<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
+				<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
+				</div>
+				 </td>
+			</tr>
+			
+			<tr   >
+				<td>
+	             <div style="padding: 5px; border-radius: 4px 4px 4px 4px; background-color: #FFFFFF;" >
+				 <a href="#"><strong>Гостевой Дом 5я Авеню </strong> </a> <br> <strong>Адрес:</strong> г.ОШ ул. Ленина 5, 714000   
+				<br> <strong>Телефон:</strong> 0555125675
+				<br> <strong>Часы работы:</strong> ежедневно, круглосуточно
+				<br> <a href="#"><strong>Фотографий </strong></a>| <a href="#"><strong>Отзывы</strong></a>
+				</div>
+				 </td>
+			</tr>
+			
+		</table>
+	</div>
 
 </div>
