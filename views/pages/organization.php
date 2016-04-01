5fb189948bd168c8d2fcd5bfab932026a88a3611
ZHoodar <ajoodar@gmail.com>
2016-04-01 12:32:53 +0600

    organization page wich will reprezent all of them

--- views/pages/organization.php
+++ views/pages/organization.php
@@ -1,8 +1,8 @@
 	
-<div id="workArea">     <!-- див рабочой зоны  -->
-		<a href="index.php?view=index">Главная </a>&rarr;<a href="index.php?view=torgovye-sentr">Торговые центры</a> &rarr;
+<div id="workArea">     	<!-- див рабочой зоны  -->
+		<a href="index.php?view=index">Главная </a>&rarr;<a href="#">Торговые центры</a> &rarr;
 		<p> <h2>  Торговый центр ОШ Сити в ОШе. Отзывы, жалобы, контакты</h2> </p>
-		<hr size="6" color="#468bea"/>
+		<hr size="6" color="#468bea" />
 		
 		<div>
 		    <strong>Адрес:</strong> г.ОШ ул. Масалиева 29 
@@ -10,27 +10,19 @@
 			<br> <strong>Часы работы:</strong> ежедневно, 09:00-22:00 
 		</div>
 
-	  <br/><br/>
+	    <br/><br/>
+		
+		<div>
+			<p >  <strong>Место нахождение на карте</strong> </p>
+			<img border="3" style="border-color: #adafb4;" src="img/map.png"/>
+		</div>
+	   <br/>
 	
-	<table border="0">
-	    <tr height="150">
-	      <td><div style="padding: 2px;"><a rel="group" href="img/oshcity/3_big.jpg" ><img src="img/oshcity/3.jpg"/></a></div></td>
-	      <td><div style="padding: 2px;"><a rel="group" href="img/oshcity/2_big.jpg" ><img src="img/oshcity/2.jpg"/></a></div></td>
-	      <td><div style="padding: 2px;"><a rel="group" href="img/oshcity/1_big.jpg" ><img src="img/oshcity/1.jpg"/></a></div></td>
-	    </tr>
-	</table>
-	
-	<div>
-	<p >  <strong>Место нахождение на карте</strong> </p>
-		<img border="3" style="border-color: #adafb4;" src="img/map.png"/>
-	</div>
-	<br/>
-	
-	<div align="justify">
-	<?
-	include("comments/comment.php");
-	?>	
-	</div>
+		<div align="justify">
+			<?
+			include("comments/comment.php");
+			?>	
+		</div>
 
 	
 </div>
