9a4be3f639455d339b8bf953e79575de37440d17
ZHoodar <ajoodar@gmail.com>
2016-04-02 17:06:19 +0600

    added conection query funktions

--- db_funs.php
+++ db_funs.php
@@ -19,9 +19,7 @@
 	function db_result_to_array($result)
     {
         $res_array = array();
-        
         $count = 0;
-        
         while($row = mysql_fetch_array($result))
         {
             $res_array[$count] = $row;
@@ -30,46 +28,121 @@
         return $res_array;
     }
 	
-	
-	function get_cat_otdyh()
+	function get_banks()
     {
         db_connect();
-        
-        $query = "SELECT * FROM ot_ra_categories ORDER BY id ";
-        
+        $query = "SELECT * FROM banks ORDER BY id ";
         $result = mysql_query($query);
-        
         $result = db_result_to_array($result);
-        
         return $result;
     }
 	
-	function get_cat_torgov()
+	function get_butik()
     {
         db_connect();
-        
-        $query = "SELECT * FROM to_us_categories ORDER BY id ";
-        
+        $query = "SELECT * FROM butik ORDER BY id ";
         $result = mysql_query($query);
-        
         $result = db_result_to_array($result);
-        
         return $result;
     }
 	
-	function get_cat_ucheb()
+	function get_cinema()
     {
         db_connect();
-        
-        $query = "SELECT * FROM uch_za_categories ORDER BY id ";
-        
+        $query = "SELECT * FROM cinemas ORDER BY id ";
         $result = mysql_query($query);
-        
         $result = db_result_to_array($result);
-        
+        return $result;
+    }
+
+	function get_hotel()
+    {
+        db_connect();
+        $query = "SELECT * FROM hotels ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
+    
+    function get_kid_garden()
+    {
+        db_connect();
+        $query = "SELECT * FROM kid_garden ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
         return $result;
     }
 	
-	
+	 function get_mall()
+    {
+        db_connect();
+        $query = "SELECT * FROM mall ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
+    
+	 function get_market()
+    {
+        db_connect();
+        $query = "SELECT * FROM markets ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
+    
+     function get_nigth_club()
+    {
+        db_connect();
+        $query = "SELECT * FROM nigthclubs ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
+    
+    function get_restobar()
+    {
+        db_connect();
+        $query = "SELECT * FROM restobars ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
+    
+    function get_school()
+    {
+        db_connect();
+        $query = "SELECT * FROM schools ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
+    
+     function get_sportclub()
+    {
+        db_connect();
+        $query = "SELECT * FROM sportclubs ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
+    
+     function get_uch_center()
+    {
+        db_connect();
+        $query = "SELECT * FROM uch_center ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
+    
+     function get_vuz()
+    {
+        db_connect();
+        $query = "SELECT * FROM vuz ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
 
 ?>
