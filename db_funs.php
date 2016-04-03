1d8bcc522fd7c6e095cdec0b52bac8c251009782
ZHoodar <ajoodar@gmail.com>
2016-04-03 18:42:32 +0600

    added the  funktions to get organization by id

--- db_funs.php
+++ db_funs.php
@@ -27,6 +27,133 @@
         }
         return $res_array;
     }
+    
+    function get_organ_bank($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM banks WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_detsad($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM kid_garden WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_cinema($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM cinemas WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_nigthclub($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM nigthclubs WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_restoran($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM restobars WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_school($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM schools WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_sportclub($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM sportclubs WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_mall($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM mall WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_uchcenter($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM uch_center WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_vuz($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM vuz WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_butik($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM butik WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_attorney($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM attorney WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	
+	 function get_organ_hotel($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM hotels WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
+	
+	function get_organ_market($id)
+    {
+		db_connect();
+		$query = ("SELECT * FROM market WHERE id='$id' ");
+		$result= mysql_query($query);
+		$row = mysql_fetch_array($result);
+		return $row;
+	}
 	
 	function get_banks()
     {
@@ -140,6 +267,14 @@
     {
         db_connect();
         $query = "SELECT * FROM vuz ORDER BY id ";
+        $result = mysql_query($query);
+        $result = db_result_to_array($result);
+        return $result;
+    }
+     function get_attorney()
+    {
+        db_connect();
+        $query = "SELECT * FROM attorney ORDER BY id ";
         $result = mysql_query($query);
         $result = db_result_to_array($result);
         return $result;
