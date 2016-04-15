8a3b2eaeb08aa4082536f462f9059bd2eaefa2ba
ZHoodar <ajoodar@gmail.com>
2016-04-15 21:39:01 +0600

    modified with the new comment session

--- organization.php
+++ organization.php
@@ -3,7 +3,6 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html>
-   
 	<head>	
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="description" content="Город Ош, справочник по городу" />
