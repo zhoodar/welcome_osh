8a301f3e74b30f049c5ba5edce16a9060c7f3831
ZHoodar <ajoodar@gmail.com>
2016-05-09 12:56:11 +0600

    added ponaram view in new page with a new style

---
+++ panorama/oshtu.php
@@ -0,0 +1,27 @@
+
+	<div style="padding: 5px;" align="center">
+		<script type="text/javascript">
+	
+			var flashvars = {};
+			var params = {};
+			params.quality = "high";
+			params.bgcolor = "#747474";
+			params.allowscriptaccess = "sameDomain";
+			params.allowfullscreen = "true";
+			params.base=".";
+			var attributes = {};
+			attributes.id = "pano";
+			attributes.name = "pano";
+			attributes.align = "middle";
+			swfobject.embedSWF(
+				"panorama/21_out.swf", "flashContent", 
+				"770", "450", 
+				"9.0.0", "expressInstall.swf", 
+				flashvars, params, attributes);
+		</script>
+		<div id="flashContent">
+			<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
+		</div>
+		
+	</div>
+
